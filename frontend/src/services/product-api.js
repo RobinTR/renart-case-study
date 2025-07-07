d0926d30b58702f24f5792c93f57e2f1c6339fde
Mem Robin Çen <memrobincen@gmail.com>
2025-07-07 20:47:22 +0300

    refactor: api url to render.com

--- frontend/src/services/product-api.js
+++ frontend/src/services/product-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://13.60.241.103/api/v1/'
+    baseURL: 'https://renart-case-study-qls3.onrender.com/api/v1/'
 });
 
 export const fetchProducts = async () => {
