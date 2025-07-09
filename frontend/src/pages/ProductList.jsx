d495354b1f859f8a3899fc98c3446dbeb705a57f
Mem Robin Çen <memrobincen@gmail.com>
2025-07-09 17:17:58 +0300

    feat: add loading spinner to ProductList component

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -3,16 +3,33 @@ import { fetchProducts } from '../services/product-api';
 import ProductCarousel from '../components/ProductCarousel';
 import styles from '../styles/product-list.module.css';
 
+import spinnerStyles from '../styles/spinner.module.css';
+
+
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetchProducts()
       .then(setProducts)
-      .catch(() => setError('Sorry, there was a problem fetching product data from the server. Please try again later.'));
+      .catch(() => setError('Sorry, there was a problem fetching product data from the server. Please try again later.'))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) {
+    return (
+      <div className={styles.container}>
+        <h2 className={styles.title}>Product List</h2>
+        <div className={spinnerStyles.spinner}>
+          <div className={spinnerStyles['spinner-circle']} />
+        </div>
+      </div>
+    );
+  }
 
   if (error) {
     return (
